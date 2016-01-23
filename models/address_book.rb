7682742cc402a2d7ee53b6cc48834d7e0e03b20b
Iggy <igoririanto@rocketmail.com>
2016-01-23 11:46:10 -0800

    Checkpoint 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -31,6 +31,29 @@ class AddressBook
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
    end
+
+   def binary_search(name)
+     lower = 0
+     upper = @entries.length - 1
+
+    # #2
+     while lower <= upper
+    # #3
+       mid = (lower + upper) / 2
+       mid_name = @entries[mid].name
+
+    # #4
+       if name == mid_name
+         return @entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     return nil #what does this display? If name==mid_name, doesn't that make return nil unnecessary?
+   end
+
 end
 
 #Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
