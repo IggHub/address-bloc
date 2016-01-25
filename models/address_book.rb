9c495316c70cfc9e0065db1df2f1663c2fe4fc85
Iggy <igoririanto@rocketmail.com>
2016-01-24 19:59:28 -0800

    Assignment 22 done

--- models/address_book.rb
+++ models/address_book.rb
@@ -11,7 +11,7 @@ class AddressBook
 
   def add_entry(name, phone_number, email)
     index = 0
-    @entries.each do |entry|
+    @entries.each do |entry| #does 'entry' refer to entry.rb?
 
       if name < entry.name
         break
@@ -30,6 +30,36 @@ class AddressBook
        row_hash = row.to_hash
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
+   end
+
+   def binary_search(name)
+     lower = 0
+     upper = @entries.length - 1
+
+     while lower <= upper
+
+       mid = (lower + upper) / 2
+       mid_name = @entries[mid].name
+
+       if name == mid_name
+         return @entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     return nil #what does this display? If name==mid_name, doesn't that make return nil unnecessary?
+   end
+
+   def iterative_search(name)
+
+     @entries.each do |lookup|
+       if lookup.name == name
+         return lookup
+       end
+     end
+     return nil
    end
 end
 
